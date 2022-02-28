a3cb33b608f451fcca7ddfcf4bf9f7aa1f4b8a59
c200643 <93572697+c200643@users.noreply.github.com>
2022-02-28 12:07:26 +0900

    Delete bg_changer.js

--- scripts/bg_changer.js
+++
@@ -1,9 +0,0 @@
-var images = [
-'../images/bg/1.jpg', 
-'../images/bg/2.jpg', 
-'../images/bg/3.jpg', 
-'../images/bg/4.jpg', 
-'../images/bg/5.jpg', 
-'../images/bg/6.jpg', 
-'../images/bg/7.jpg'];
-$('html').css({'background-image': 'url(images/' + images[Math.floor(Math.random() * images.length)] + ')'});
